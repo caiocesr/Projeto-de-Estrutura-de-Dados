85c9c34916e067bb740bf95ab19cf0f910c1b27d
Caio César Barbosa <167909208+caiocesr@users.noreply.github.com>
2025-06-18 10:51:37 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
 package view;
 
-import view.MainView;
-
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new MainView();
-        });
+        SimuladorGUI.main(args);
     }
 }
